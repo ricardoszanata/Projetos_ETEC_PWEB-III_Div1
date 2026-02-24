327eee0540f4595b46df0814683be6404a4eb455
ricardoszanata <ricardoszanata@gmail.com>
2026-02-24 09:01:53 -0300

    inicio da construção da API

---
+++ banco.sql
@@ -0,0 +1,9 @@
+create database lanchonete;
+use lanchonete;
+create table usuarios(
+    usuid int primary key auto_increment,
+    usunome varchar(100),
+    usulogin varchar(100),
+    ususenha varchar(100),
+    usulogado boolean default 0
+    );
